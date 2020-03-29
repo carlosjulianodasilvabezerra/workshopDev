8b8375a40e8fe52e208df7443fb0d8a43a66c4a4
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2020-03-29 15:26:20 -0300

    reasuste no codigo

--- casaCriativa/scripts.js
+++ casaCriativa/scripts.js
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
 document.querySelector('button.fat')
   .addEventListener('click', function() {
     document.querySelector('#footer').classList.toggle('hide')
   })
-=======
-<<<<<<< HEAD
-document.querySelector('button.fat')
-  .addEventListener('click', function() {
-    document.querySelector('#footer').classList.toggle('hide')
-  })
-=======
+
 function onOff() {
   document.querySelector('#modal').classList.toggle('hide')
 }
-
-
-
-
-
-// document.querySelector('button.fat')
-//   .addEventListener('click', onOff)
->>>>>>> commit house
->>>>>>> commit in house
