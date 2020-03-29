938dcaa5fbf53c6c3a5f39f0062a3dd0487bb22b
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2020-03-29 12:25:06 -0300

    commit in house

---
+++ casaCriativa/scripts.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+document.querySelector('button.fat')
+  .addEventListener('click', function() {
+    document.querySelector('#footer').classList.toggle('hide')
+  })
+=======
+function onOff() {
+  document.querySelector('#modal').classList.toggle('hide')
+}
+
+
+
+
+
+// document.querySelector('button.fat')
+//   .addEventListener('click', onOff)
+>>>>>>> commit house
